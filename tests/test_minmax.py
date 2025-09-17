8f63b913ab578d67999c81fae8da789c22df9d05
François Boulogne <devel@sciunto.org>
2025-09-17 08:56:28 +0200

    add unittest for ransac method

--- tests/test_minmax.py
+++ tests/test_minmax.py
@@ -121,3 +121,21 @@ def test_minmax_linreg_data_basic(test_data_dir):
     assert_allclose(result.thickness, expected, rtol=1e-1)
 
 
+def test_minmax_linreg_data_basic(test_data_dir):
+    spectrum_path = test_data_dir / 'basic' / '000004310.xy'
+    expected = 1338.35
+
+    lambdas, raw_intensities = load_spectrum(spectrum_path, wavelength_min=450)
+    smoothed_intensities = smooth_intensities(raw_intensities)
+    r_index =  n_lambda(lambdas)
+
+    prominence = 0.02
+
+    result = thickness_from_minmax(lambdas,
+                                   smoothed_intensities,
+                                   refractive_index=r_index,
+                                   min_peak_prominence=prominence,
+                                   method='ransac',
+                                   plot=False)
+
+    assert_allclose(result.thickness, expected, rtol=1e-1)
